1466d979dde2a54ead1d1ef7e7464ff67e0e0004
konnichiwaeien <konnichiwaeien@gmail.com>
2026-03-17 17:14:56 +0500

    feat: change btn text

--- app/components/form-donation/form-donation.tsx
+++ app/components/form-donation/form-donation.tsx
@@ -484,7 +484,7 @@ export const FormDonation = ({ className, onClose, initialCampaignId, initialCam
             disabled={isLoading || !isValid}
             className="w-full rounded-xl md:rounded-2xl bg-brand-brown py-5 md:py-6 text-center text-xl md:text-2xl font-black uppercase tracking-widest text-white transition-all hover:bg-brand-orange disabled:opacity-50 disabled:cursor-not-allowed flex items-center justify-center cursor-pointer shadow-xl shadow-brand-brown/20"
           >
-            {isLoading ? <Loader2 className="animate-spin h-8 w-8" /> : (isRecurring ? 'Оформить подписку' : 'Помочь')}
+            {isLoading ? <Loader2 className="animate-spin h-8 w-8" /> : (isRecurring ? 'Подписка на помощь' : 'Помочь')}
           </button>
         </div>
 
